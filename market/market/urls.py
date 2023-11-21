da7f33f1465bdf57d0138961a54f9c40cb0d6954
govind ratnakar naik <govindrnaik.trk@gmail.com>
2023-11-22 00:03:17 +0530

    working on contact

--- market/market/urls.py
+++ market/market/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
 
+from core.views import index, contact
 urlpatterns = [
-    path('', index),
-    path('admin/', admin.site.urls),
-
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('admin/', view=admin.site.urls),
 ]
